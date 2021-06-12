3c471f56ea44c10d023c31d90a60c180ea40efc8
pierrebeaulieu <pierre.beaulieu@mines-paristech.fr>
2021-06-12 18:06:21 +0200

    drag and drop

--- cas_base.py
+++ cas_base.py
@@ -4,12 +4,19 @@ import streamlit as st
 import pandas as pd
 import matplotlib.pyplot as plt
 import numpy as np
+import pandas as pd
+import base64
+import io
 from copy import deepcopy
 
 # Pour récupérer les données
 import numpy as np
 import matplotlib.pyplot as plt
 import csv
+
+
+""" GROS PROBLEME AVEC LES ANNEES """
+
 
 # n est les nombre d'années sur lesquelles on veut effectuer le calcul
 
@@ -261,64 +268,139 @@ def app():
             return Tri
 
 
-    n=1
-    # on extrait les données du tableau de base et modifie en fonction du choix du client
-    valeurs = entree(extraction_csv("donnees_entree_proj_minier.csv", n))
+    
 
 
 
-    st.markdown("# 1-Affichage des données # ")
+    st.markdown("# 1 - Affichage des données # ")
 
     st.text(" ")   # Pour sauter une ligne, sinon le texte se superpose
     st.text(" ")
     st.text(' ')
 
+    type_entree = st.selectbox("Rentrer les données à la main ou par un .csv", [".csv", "Manuellement"], )
 
-    if st.checkbox("Voir les données d'entrées", value = True): 
+    if type_entree == ".csv":
+
+        st.write(f'Voici le template à télécharger pour renseigner les valeurs en entrée si vous ne le possédez pas déjà')
+
+        download=st.button('Télécharger le Template')
+
+        if download:
+            df = pd.read_csv("donnees_entree_proj_minier.csv")
+            fichiercsv = df.to_csv(index=False)
+            b64 = base64.b64encode(fichiercsv.encode()).decode()  # some strings
+            linko= f'<a href="data:file/csv;base64,{b64}" download="myfilename.csv">Download csv file</a>'
+            st.markdown(linko, unsafe_allow_html=True)
+
+        st.write(f'Veuillez renseigner le chemin accès à votre .csv pour continuer.')
+        st.write(f'Pour se faire, faites un clique droit sur votre .csv, puis cliquez sur propriétés, puis copiez le chemin et ajoutez /nomfichier.csv')
+
+        file_path= st.text_input('Chemin accès du fichier')
+
+        if len(file_path) >1:
+
+            st.text(" ")   # Pour sauter une ligne, sinon le texte se superpose
+            st.text(" ")
+            st.text(' ')
+
+        
+
+            #détermine le nombre d'années
+
+            annees = st.number_input("nombre d'années considérées", 0, 50, 15)
+            valeurs = entree(extraction_csv(file_path, annees))
+            n = annees
+            if st.checkbox("Voir les données d'entrées", value = True): 
+                st.subheader("Données d'entrée")
+                st.markdown(valeurs)
+
+            st.markdown(
+                "### Tracé de la valeur choisie en fonction du paramètre choisi ### \n  \n \n  ")
+            st.markdown("\n \n ")
+            
+            # Ce que l'on doit tracer en fonction des années
+
+            sortie = st.selectbox("Axe des ordonnées", ['cash_flow_actu', 'tri', "cumul_cash_flow_actu"], )
+
+
+
+            # tableau d'entrée avec le bon nombre d'années
+            
+            abscisse = np.arange(1,annees)
+            evaluable = "valeurs."+f"{sortie}"+"()"
+            ordonne = eval(evaluable)[1:]
+            fig, ax = plt.subplots()
+            plt.style.use('seaborn')  # pour avoir un autre style de graphique plus frais
+            
+            st.write(f" Le cumul des cash flow avec les données initiales vaut {valeurs.cumul_cash_flow_actu()[-1] :.2f} M$ après l'année {n-1}")
+            #tracé avec les valeurs initiales
+            ax = plt.scatter(abscisse, ordonne,
+                            label=f"valeur initiale")
+
+            plt.ylabel('$ (en millions) ')
+            plt.xlabel("annee")
+            plt.title(f"{sortie}")
+            plt.legend()
+
+            st.pyplot(fig)
+    else :
+        st.text(" ")   # Pour sauter une ligne, sinon le texte se superpose
+        st.text(" ")
+        st.text(' ')
+
+
+
+        #détermine le nombre d'années
+
+        annees = st.number_input("nombre d'années considérées", 0, 50, 15)
+        valeurs = entree(extraction_csv("donnees_entree_proj_minier.csv", annees))
+        n = annees
+        
         st.subheader("Données d'entrée")
-        st.markdown(valeurs)
+        valeurs.n = annees
+        valeurs.investissement = np.array([st.number_input('investissement')]*valeurs.n)
+        valeurs.cout_tonne_remuee = np.array([st.number_input('cout_tonne_remuee')]*valeurs.n)
+        valeurs.ratio_sterile = np.array([st.number_input('ratio_sterile')]*valeurs.n)
+        valeurs.cout_traitement = np.array([st.number_input('cout_traitement')]*valeurs.n)
+        valeurs.charges_fixes = np.array([st.number_input('charges_fixes')]*valeurs.n)
+        valeurs.prix_or = np.array([st.number_input('prix_or')]*valeurs.n)
+        valeurs.taux_recuperation_or = np.array([st.number_input('taux_recuperation_or')]*valeurs.n)
+        valeurs.prop_or_paye_dore = np.array([st.number_input('prop_or_paye_dore')]*valeurs.n)
+        valeurs.nombre_grammes_once = np.array([st.number_input('nombre_grammes_once')]*valeurs.n)
+        valeurs.taux_actualisation = np.array([st.number_input('taux_actualisation')]*valeurs.n)
+        valeurs.tonnage_geol = np.array([st.number_input('tonnage_geol')]*valeurs.n)
+        valeurs.teneur_minerai_geol = np.array([st.number_input('teneur_minerai_geol')]*valeurs.n)
+        valeurs.taux_recup = np.array([st.number_input('taux_recup')]*valeurs.n)
+        valeurs.dilution_minerai = np.array([st.number_input('dilution_minerai')]*valeurs.n)
+        valeurs.rythme_prod_annee = np.array([st.number_input('rythme_prod_annee')]*valeurs.n)
 
+        st.markdown(
+            "### Tracé de la valeur choisie en fonction du paramètre choisi ### \n  \n \n  ")
+        st.markdown("\n \n ")
+        
+        # Ce que l'on doit tracer en fonction des années
 
-    #détermine le nombre d'années
-
-    annees = st.number_input("nombre d'années considérées", 0, 50, 15)
-
-    st.markdown(
-        "### Tracé de la valeur choisie en fonction du paramètre choisi ### \n  \n \n  ")
-    st.markdown("\n \n ")
-    
-
-    sortie = st.selectbox("Axe des ordonnées", ['cash_flow_actu', 'tri', "cumul_cash_flow_actu","cout_exploitation_tonne_minerai","teneur_minerai_industriel","tonnage_industriel","qte_or_paye","recette_tonne_minerai","dep_operatoiresMCO","dep_operatoiresTraitement","dep_operatoiresTotale","recette","taux_actualisation_"], )
-    inputt = st.selectbox("Axe des abcisses",['annee'])
-
-
-    # paramètres à modifier par le client
-
-    # tableau d'entrée avec le bon nombre d'années
-    valeurs = entree(extraction_csv("donnees_entree_proj_minier.csv", annees))
-    evaluable = "valeurs."+f"{n}"
-    abscisse = np.arange(1,annees)
-    evaluable = "valeurs."+f"{sortie}"+"()"
-    ordonne = eval(evaluable)[1:]
-  
-    st.markdown(
-        str(len(abscisse))+str(len(ordonne)))
-    st.markdown("\n \n ")
+        sortie = st.selectbox("Axe des ordonnées", ['cash_flow_actu', 'tri', "cumul_cash_flow_actu"], )
 
 
 
+        # tableau d'entrée avec le bon nombre d'années
+        
+        abscisse = np.arange(1,annees)
+        evaluable = "valeurs."+f"{sortie}"+"()"
+        ordonne = eval(evaluable)[1:]
+        fig, ax = plt.subplots()
+        plt.style.use('seaborn')  # pour avoir un autre style de graphique plus frais
+        
+        st.write(f" Le cumul des cash flow avec les données initiales vaut {valeurs.cumul_cash_flow_actu()[-1] :.2f} M$ après l'année {n-1}")
+        #tracé avec les valeurs initiales
+        ax = plt.scatter(abscisse, ordonne,
+                        label=f"valeur initiale")
 
-    fig, ax = plt.subplots()
-    plt.style.use('seaborn')  # pour avoir un autre style de graphique plus frais
-    sortie = eval(evaluable)
-    st.write(f" Le cumul des cash flow avec les données initiales vaut {valeurs.cumul_cash_flow_actu()[-1] :.2f} M$ après l'année {n-1}")
-    #tracé avec les valeurs initiales
-    ax = plt.scatter(abscisse, ordonne,
-                    label=f"valeur initiale")
+        plt.ylabel('$ (en millions) ')
+        plt.xlabel("annee")
+        plt.title(f"{sortie}")
+        plt.legend()
 
-    plt.ylabel('$ (en millions) ')
-    plt.xlabel("annee")
-    plt.title(f"{sortie}")
-    plt.legend()
-
-    st.pyplot(fig)
+        st.pyplot(fig) 
